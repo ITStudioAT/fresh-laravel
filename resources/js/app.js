28925e6dbba57ea62a5130bea828e5bbf5cce407
Guenther Kron <kron@naturwelt.at>
2025-04-14 11:29:42 +0200

    bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,2 @@
-import { createApp } from 'vue';
-import FreshLaravelComponent from './components/FreshLaravelComponent.vue';
-
-const app = createApp({});
-app.component('fresh-laravel-component', FreshLaravelComponent);
-app.mount('#app');
+import './bootstrap';
+//var KUTE = require("kute.js");
